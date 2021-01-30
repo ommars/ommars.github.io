263136dd390325d8e1a03fa5efabd2f67515ad3e
ommars <omardenilson007@gmail.com>
2021-01-30 12:47:43 -0500

    Add animations, dark mode, and fixed header

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log("Succesfully Connected");
+let tema = document.getElementById("theme");
+let boton = document.getElementById("trigger");
+console.log(boton.innerHTML)
+
+boton.addEventListener("click", function cambiarTema() {
+    if (tema.getAttribute("href") == "./styles-day.css" ){
+        tema.href = "./styles-night.css"
+        boton.innerText = "Day";
+    }else{
+        tema.href="./styles-day.css"
+        boton.innerText = "Night";
+
+    }
+});
