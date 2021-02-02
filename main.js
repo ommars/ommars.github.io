5b872c4ea5ae3388491c27329bc0e09429bd3f48
ommars <omardenilson007@gmail.com>
2021-02-02 12:04:45 -0500

    PC styles added

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let tema = document.getElementById("theme");
 let boton = document.getElementById("trigger");
+let xD = document.getElementById("menuL");
 
 boton.addEventListener("click", function cambiarTema() {
     if (tema.getAttribute("href") == "./styles-day.css" ){
@@ -9,5 +10,13 @@ boton.addEventListener("click", function cambiarTema() {
         tema.href="./styles-day.css"
         boton.innerText = "Night";
 
+    }
+});
+
+xD.addEventListener("click", function cambiarIcon() {
+    if(xD.innerHTML == " ☰ "){
+        xD.innerHTML = " ✖ "
+    } else {
+        xD.innerHTML = " ☰ "
     }
 });
