bbc3e2c76e0890793981c1ff06d79fe25f318b2e
ommars <omardenilson007@gmail.com>
2021-02-10 16:55:17 -0500

    Rediseño minimalista

--- main.js
+++ main.js
@@ -1,17 +1,4 @@
-let tema = document.getElementById("theme");
-let boton = document.getElementById("trigger");
 let xD = document.getElementById("menuL");
-
-boton.addEventListener("click", function cambiarTema() {
-    if (tema.getAttribute("href") == "./styles-day.css" ){
-        tema.href = "./styles-night.css"
-        boton.innerText = "Day";
-    }else{
-        tema.href="./styles-day.css"
-        boton.innerText = "Night";
-
-    }
-});
 
 xD.addEventListener("click", function cambiarIcon() {
     if(xD.innerHTML == " ☰ "){
